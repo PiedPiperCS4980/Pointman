5f340aa20c45f4bfa3573f52342686937ca84b9f
huntergolden <hrggyf@mail.missouri.edu>
2016-04-26 12:12:57 -0500

    Fixing sensitivity, remove debug text

--- Build/Android/src/com/YourCompany/Pointman/OBBData.java
+++ Build/Android/src/com/YourCompany/Pointman/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-145921506L // the length of the file in bytes
+145913672L // the length of the file in bytes
 )
 };
 };
